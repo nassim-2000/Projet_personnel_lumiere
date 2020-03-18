c2e0cef0f666eabb74628a10dca6638573a6c602
nassim-2000 <60080248+nassim-2000@users.noreply.github.com>
2020-03-18 18:48:06 +0100

    Projet Lumières

---
+++ pilotage_prise_emetteur.cpp
@@ -0,0 +1,28 @@
+/*====================================================
+*  Programme permettant de piloter des prises
+*  télécommandées à l'aide d'émetteur 433Mhz
+*====================================================*/
+
+/*bibliothèques requises*/
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+Servo monServo;
+
+const int gpio = 8;   //déclaration pin 8 --> sortie gpio du esp8266
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(gpio,INPUT);    //déclarer pin 8 (sortie esp8266) --> entrée
+  mySwitch.enableTransmit(10);   //Emetteur branché sur pin 10
+}
+
+
+void loop() {
+/*pilotage du de la prise télécommandée à l'aide de l'envoi du signal*/
+  mySwitch.send( 5510485, 24);    //envoi trame du signal prise --> ON
+  delay(2000);
+  mySwitch.send( 5510657, 24);    //envoi trame du signal prise --> OFF
+  delay(2000);
+}
